81af40a659f1e78e19e65352f4c45de1bb7f6a95
Irfan Aziz Al Amin <irfan.aziz@mamiteam.com>
2024-01-23 19:11:04 +0700

    update soal 1

--- soal1.js
+++ soal1.js
@@ -1,2 +1,11 @@
 // TODO: bikin kondisional
 // nilai A, B, C
+
+/**
+ * Test Cases:
+    - dibawah 55 (ga lulus)
+    - ketika nilainya 55-70 (C)
+    - ketika nilainya 71-80 (B)
+    - ketika nilainya 81-85 (A)
+    - ketika nilainya 86> (A+)
+ */
